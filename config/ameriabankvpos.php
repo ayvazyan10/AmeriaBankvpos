e1f2d16f96003a94a0e683ba6a776bbd1b193b56
razmikayvazyan <ayvazyan10@gmail.com>
2023-04-21 12:38:12 +0400

    Init Commit

--- config/ameriabankvpos.php
+++ config/ameriabankvpos.php
@@ -4,7 +4,8 @@ return [
     "ClientID" => '00000000-0000-0000-0000-000000000000', // provided by bank
     "Username" => "00000000_api", // provided by bank
     "Password" => "0000000000000000", // provided by bank
-    "Currency" => "051", // currency ISO code (current: AMD)
+    "Language" => "am", // Interface language: am-Armenian ru-Russian en-English
+    "Currency" => "051", // currency ISO code ((051 - AMD)-(by default) (978 - EUR) (840 - USD) (643 - RUB))
     "TestMode" => false, // true|false
     "BackUrl" => 'order.payment.check', // (route: name) redirect back url from bank - success|fail
 ];
--- src/AmeriaBankVPOS.php
+++ src/AmeriaBankVPOS.php
@@ -16,47 +16,56 @@ class AmeriaBankVPOS
     private string $username;
     private string $password;
     private string $backUrl;
-    private bool $testMode;
     private string $currency;
     private string $mode;
     private string $language;
 
+    /**
+     * AmeriaBankVPOS constructor.
+     */
     public function __construct()
     {
         $this->clientId = config('ameriabankvpos.ClientID');
         $this->username = config('ameriabankvpos.Username');
         $this->password = config('ameriabankvpos.Password');
         $this->backUrl = route(config('ameriabankvpos.BackUrl'));
-        $this->testMode = config('ameriabankvpos.TestMode');
         $this->currency = config('ameriabankvpos.Currency');
         $this->language = config('ameriabankvpos.Language');
         $this->mode = config('ameriabankvpos.TestMode') ? 'test' : '';
     }
 
     /**
+     * Process the payment and redirect to AMERIABANK payment interface.
+     *
      * @param int|float $amount // Amount to Charge
      * @param int $orderId // Payment Unique Order ID
      * @param string|null $description // Description for payment
+     * @param string|null $currency // Currency to be used for the transaction
      * @param string|null $language // Language to display on AMERIABANK payment interface
-     * @return void // Redirecting or throwing Exception
-     * @throws Exception
+     * @throws Exception // If any error occurs during the API request
      */
-    public function pay(int|float $amount, int $orderId, string $description = null, string $language = null): void
+    public function pay(
+        int|float $amount,
+        int $orderId,
+        string $description = null,
+        string $currency = null,
+        string $language = null
+    ): void
     {
-        $args = [
+        $parameters = [
             "ClientID" => $this->clientId,
             "BackURL" => $this->backUrl,
             "Username" => $this->username,
             "Password" => $this->password,
-            "TestMode" => $this->testMode,
-            "Currency" => $this->currency,
+            "Language" => $language === null ? $this->language : $language,
+            "Currency" => $currency === null ? $this->currency : $currency,
             "Amount" => $amount,
             "OrderID" => $orderId,
             "Description" => $description,
         ];
 
         try {
-            $client = Http::post("https://services{$this->mode}.ameriabank.am/VPOS/api/VPOS/InitPayment", $args);
+            $client = Http::post("https://services{$this->mode}.ameriabank.am/VPOS/api/VPOS/InitPayment", $parameters);
         } catch (Exception $e) {
             throw new Exception(self::PROVIDER . ' API error: ' . $e->getMessage());
         }
@@ -71,11 +80,13 @@ class AmeriaBankVPOS
     }
 
     /**
-     * @param $request
-     * @return string
-     * @throws Exception
+     * Check the payment status and return the transaction details.
+     *
+     * @param $request // Incoming request data
+     * @return array // Array containing transaction status and details
+     * @throws Exception // If any error occurs during the API request
      */
-    public function check($request): string
+    public function check($request): array
     {
         $order_id = $request->input('orderID');
         $payment_id = $request->input('paymentID');
@@ -84,31 +95,64 @@ class AmeriaBankVPOS
             throw new Exception(self::PROVIDER . ' API error: Empty OrderID field');
         }
 
-        $args = [
+        $parameters = [
             "PaymentID" => $payment_id,
             "Username" => $this->username,
             "Password" => $this->password,
         ];
 
+        $response = $this->sendRequest("https://services{$this->mode}.ameriabank.am/VPOS/api/VPOS/GetPaymentDetails", $parameters);
+
+        $transaction = $this->saveTransaction($response, $payment_id);
+
+        if ($response["ResponseCode"] != '00') {
+            return [
+                "status" => "FAIL",
+                "transaction" => $transaction
+            ];
+        }
+
+        return [
+            "status" => "SUCCESS",
+            "transaction" => $transaction
+        ];
+    }
+
+    /**
+     * Send an HTTP request to the given URL with the provided arguments.
+     *
+     * @param string $url // URL to send the request to
+     * @param array $parameters // Arguments to be sent in the request body
+     * @return array // Response data as an associative array
+     * @throws Exception // If any error occurs during the API request
+     */
+    private function sendRequest(string $url, array $parameters): array
+    {
         try {
-            $client = Http::post("https://services{$this->mode}.ameriabank.am/VPOS/api/VPOS/GetPaymentDetails", $args);
+            $client = Http::post($url, $parameters);
         } catch (Exception $e) {
             throw new Exception(self::PROVIDER . ' API error: ' . $e->getMessage());
         }
 
-        $response = json_decode($client, true);
+        return json_decode($client->body(), true);
+    }
 
+    /**
+     * Save the transaction details to the database.
+     *
+     * @param array $response // Response data from the API
+     * @param string $payment_id // Payment ID received from the API
+     * @return AmeriaBankTransaction // Saved transaction object
+     */
+    private function saveTransaction(array $response, string $payment_id): AmeriaBankTransaction
+    {
         $transaction = new AmeriaBankTransaction;
         $transaction->order_id = $response["OrderID"];
-        $transaction->user_id = auth()->user()->id ? auth()->user()->id : null;
+        $transaction->user_id = optional(auth()->user())->id;
         $transaction->payment_id = $payment_id;
         $transaction->Provider = self::PROVIDER;
         $transaction->fill($response)->save();
 
-        if ($response["ResponseCode"] != '00') {
-            return 'FAIL';
-        }
-
-        return 'SUCCESS';
+        return $transaction;
     }
 }
