42853ea7e028e11218154e89d47bcaa24fe6d21f
razmikayvazyan <ayvazyan10@gmail.com>
2023-04-24 23:58:39 +0400

    comments clean

--- src/AmeriaBankVPOSServiceProvider.php
+++ src/AmeriaBankVPOSServiceProvider.php
@@ -13,10 +13,7 @@ class AmeriaBankVPOSServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'ayvazyan10');
-        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'ayvazyan10');
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
-        // $this->loadRoutesFrom(__DIR__.'/routes.php');
 
         // Publishing is only necessary when using the CLI.
         if ($this->app->runningInConsole()) {
@@ -66,23 +63,5 @@ class AmeriaBankVPOSServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__ . '/../database/migrations' => database_path('migrations'),
         ], 'ameriabankvpos.migrations');
-
-        // Publishing the views.
-        /*$this->publishes([
-            __DIR__.'/../resources/views' => base_path('resources/views/vendor/ayvazyan10'),
-        ], 'ameriabankvpos.views');*/
-
-        // Publishing assets.
-        /*$this->publishes([
-            __DIR__.'/../resources/assets' => public_path('vendor/ayvazyan10'),
-        ], 'ameriabankvpos.views');*/
-
-        // Publishing the translation files.
-        /*$this->publishes([
-            __DIR__.'/../resources/lang' => resource_path('lang/vendor/ayvazyan10'),
-        ], 'ameriabankvpos.views');*/
-
-        // Registering package commands.
-        // $this->commands([]);
     }
 }
