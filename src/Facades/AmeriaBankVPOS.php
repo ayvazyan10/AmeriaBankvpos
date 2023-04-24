773d990f9fca3e957fbd398c4dbe9c80071c0438
razmikayvazyan <ayvazyan10@gmail.com>
2023-04-24 19:50:32 +0400

    deleted not used lines

--- src/Facades/AmeriaBankVPOS.php
+++ src/Facades/AmeriaBankVPOS.php
@@ -2,12 +2,8 @@
 
 namespace Ayvazyan10\AmeriaBankVPOS\Facades;
 
-use Ayvazyan10\AmeriaBankVPOS\AmeriaBankVPOS;
 use Illuminate\Support\Facades\Facade;
-/**
- *
- * @see AmeriaBankVPOS
- */
+
 class AmeriaBankVPOS extends Facade
 {
     protected static function getFacadeAccessor(): string
